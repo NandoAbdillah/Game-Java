70c4a5c0dbf6521221b6b57c102d514d6e8a0b09
Nando Abdillah <140145973+NandoAbdillah@users.noreply.github.com>
2026-05-16 23:14:02 +0700

    update android launcher allowed fullscreen policy

--- android/src/main/java/com/bismillahjuara/game/android/AndroidLauncher.java
+++ android/src/main/java/com/bismillahjuara/game/android/AndroidLauncher.java
@@ -1,18 +1,33 @@
 package com.bismillahjuara.game.android;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 import com.bismillahjuara.game.Main;
 
-/** Launches the Android application. */
 public class AndroidLauncher extends AndroidApplication {
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate (Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        AndroidApplicationConfiguration configuration = new AndroidApplicationConfiguration();
-        configuration.useImmersiveMode = true; // Recommended, but not required.
-        initialize(new Main(), configuration);
+
+        // Konfigurasi LibGDX untuk Android
+        AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+
+        // Navigation Bar dan Status Bar (baterai/sinyal) hilang (Mode Immersive)
+        config.useImmersiveMode = true;
+
+        // KUNCI FULLSCREEN T
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            WindowManager.LayoutParams layoutParams = getWindow().getAttributes();
+            // Mengizinkan game merender grafis menembus area poni/kamera depan layar
+            layoutParams.layoutInDisplayCutoutMode = WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES;
+            getWindow().setAttributes(layoutParams);
+        }
+
+        // 3. Jalankan game-nya!
+        initialize(new Main(), config);
     }
 }
