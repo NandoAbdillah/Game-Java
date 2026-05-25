7f128c9e2e7669f1ddc7129a9429be586c2071b0
Nando Abdillah <140145973+NandoAbdillah@users.noreply.github.com>
2026-05-25 21:49:09 +0700

    add gamecontext

--- core/src/main/java/com/bismillahjuara/game/core/GameContext.java
+++ core/src/main/java/com/bismillahjuara/game/core/GameContext.java
@@ -7,7 +7,7 @@ import com.bismillahjuara.game.managers.WorldManager;
 import com.bismillahjuara.game.entity.Player;
 
 /**
- * Jantung Komunikasi Gameplay.
+ Komunikasi Gameplay.
  */
 public class GameContext {
     public AdvancedCameraSystem camera;
@@ -16,7 +16,7 @@ public class GameContext {
 
     public WorldManager worldManager;
     public EntityManager entityManager;
-    public SceneRenderer sceneRenderer; // FIX: Wajib ada agar entitas bisa mendaftarkan 3D modelnya
+    public SceneRenderer sceneRenderer;
 
     public GameplayState state = GameplayState.INITIALIZING;
     public final float fixedTimeStep = 1f / 60f;
